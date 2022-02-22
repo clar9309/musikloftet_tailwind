594936c90256b35e8ed24b51417ff8826deaf55d
Emilie Schultz <emil07v9@stud.kea.dk>
2022-02-22 14:07:54 +0100

    opstart_linksider

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+      lavendel: "#C8B1F3",
+      cream: "#FBFBFB",
+      dark: "#232326",
+    },
+    extend: {
+      fontFamily: {
+        carme: "'Carme', sans serif",
+        noto: "'Noto Serif', serif",
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
