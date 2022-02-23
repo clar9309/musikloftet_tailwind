70d23221cf1618e27d16d6d95e9105f1393b19d6
Emilie Schultz <emil07v9@stud.kea.dk>
2022-02-23 10:53:04 +0100

    js merge

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 const btn = document.querySelector("button.menu-button");
 const menu = document.querySelector(".mobile-menu");
 btn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
->>>>>>> cb5f2533e535777dd724ff53246eccaf1a541d54
