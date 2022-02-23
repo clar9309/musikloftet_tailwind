fe965361bca71fdcc5065f6ce6d7ca7833649fa7
“clar9309” <clar9309@stud.kea.dk>
2022-02-23 10:27:01 +0100

    burger menu / virker ikke

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-
+const btn = document.querySelector("button.menu-button");
+const menu = document.querySelector(".mobile-menu");
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
