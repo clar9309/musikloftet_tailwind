ba679161dfe6a577a05311c4edd2a08f2d72d678
Emilie Schultz <emil07v9@stud.kea.dk>
2022-02-25 11:31:00 +0100

    klaverlyde

---
+++ src/index.js
@@ -0,0 +1,98 @@
+const { doc } = require("prettier");
+
+//KLAVER knapper
+const knap1 = document.querySelector(".knap1");
+const knap2 = document.querySelector(".knap2");
+const knap3 = document.querySelector(".knap3");
+const knap4 = document.querySelector(".knap4");
+const knap5 = document.querySelector(".knap5");
+const knap6 = document.querySelector(".knap6");
+const knap7 = document.querySelector(".knap7");
+const knap8 = document.querySelector(".knap8");
+const knap9 = document.querySelector(".knap9");
+const knap10 = document.querySelector(".knap10");
+const knap11 = document.querySelector(".knap11");
+const knap12 = document.querySelector(".knap12");
+const knap13 = document.querySelector(".knap13");
+const knap14 = document.querySelector(".knap14");
+const knap15 = document.querySelector(".knap15");
+const knap16 = document.querySelector(".knap16");
+const knap17 = document.querySelector(".knap17");
+const knap18 = document.querySelector(".knap18");
+const knap19 = document.querySelector(".knap19");
+const knap20 = document.querySelector(".knap20");
+const knap21 = document.querySelector(".knap21");
+const knap22 = document.querySelector(".knap22");
+const knap23 = document.querySelector(".knap23");
+const knap24 = document.querySelector(".knap24");
+const knap25 = document.querySelector(".knap25");
+const knap26 = document.querySelector(".knap26");
+const knap27 = document.querySelector(".knap27");
+const knap28 = document.querySelector(".knap28");
+const knap29 = document.querySelector(".knap29");
+const knap30 = document.querySelector(".knap30");
+const knap31 = document.querySelector(".knap31");
+const knap32 = document.querySelector(".knap32");
+const knap33 = document.querySelector(".knap33");
+
+// Lyde oprettet
+const lyd1 = document.querySelector("#lyd1");
+const lyd2 = document.querySelector("#lyd2");
+const lyd3 = document.querySelector("#lyd3");
+const lyd4 = document.querySelector("#lyd4");
+const lyd5 = document.querySelector("#lyd5");
+
+// Lyt efter hover
+knap1.addEventListener("mouseover", startLyd1);
+knap2.addEventListener("mouseover", startLyd2);
+knap3.addEventListener("mouseover", startLyd3);
+knap4.addEventListener("mouseover", startLyd4);
+knap5.addEventListener("mouseover", startLyd5);
+knap6.addEventListener("mouseover", startLyd1);
+knap7.addEventListener("mouseover", startLyd2);
+knap8.addEventListener("mouseover", startLyd3);
+knap9.addEventListener("mouseover", startLyd4);
+knap10.addEventListener("mouseover", startLyd5);
+knap11.addEventListener("mouseover", startLyd1);
+knap12.addEventListener("mouseover", startLyd2);
+knap13.addEventListener("mouseover", startLyd3);
+knap14.addEventListener("mouseover", startLyd4);
+knap15.addEventListener("mouseover", startLyd5);
+knap16.addEventListener("mouseover", startLyd1);
+knap17.addEventListener("mouseover", startLyd2);
+knap18.addEventListener("mouseover", startLyd3);
+knap19.addEventListener("mouseover", startLyd4);
+knap20.addEventListener("mouseover", startLyd5);
+knap21.addEventListener("mouseover", startLyd1);
+knap22.addEventListener("mouseover", startLyd2);
+knap23.addEventListener("mouseover", startLyd3);
+knap24.addEventListener("mouseover", startLyd4);
+knap25.addEventListener("mouseover", startLyd5);
+knap26.addEventListener("mouseover", startLyd1);
+knap27.addEventListener("mouseover", startLyd2);
+knap28.addEventListener("mouseover", startLyd3);
+knap29.addEventListener("mouseover", startLyd4);
+knap30.addEventListener("mouseover", startLyd5);
+knap31.addEventListener("mouseover", startLyd5);
+knap32.addEventListener("mouseover", startLyd5);
+knap33.addEventListener("mouseover", startLyd5);
+
+function startLyd1() {
+  document.querySelector("#lyd1").play();
+}
+
+function startLyd2() {
+  document.querySelector("#lyd2").play();
+}
+
+function startLyd3() {
+  document.querySelector("#lyd3").play();
+}
+
+function startLyd4() {
+  document.querySelector("#lyd4").play();
+}
+
+function startLyd5() {
+  document.querySelector("#lyd5").play();
+}
